591e920ec451f52ec7bf6279af1255b089a6b6bf
vishalkrochwani <158709336+vishalkrochwani@users.noreply.github.com>
2025-02-08 16:22:51 +0500

    COMMIT

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { client } from "@/sanity/lib/client";
 import Image from "next/image";
 import { urlFor } from "@/sanity/lib/image";
 import Swal from "sweetalert2";
-import ProtectedRoute from "@/app/components/protected/page";
+import ProtectedRoute from "@/app/components/ProtectedRoute";
 
 interface Order {
   _id: string;
